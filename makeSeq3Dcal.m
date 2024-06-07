5f2fd7d906c9ef231c1c54bbd47eaad33317dee9
rextlfung <rexfung@umich.edu>
2024-06-07 16:10:57 -0400

    Split sequence into calibration portion and looping portion for GE scanners. Changed recon script to use scan archive files instead of p-files

---
+++ makeSeq3Dcal.m
@@ -0,0 +1,315 @@
+% brainstem interleaved 3D-EPI sequence in Pulseq, calibration portion
+%
+% This short sequence first excites the volume to steady state, then 
+% acquires many readout lines without Gy and Gz blips to:
+% 1. Allow the scanner to tune receiver gains
+% 2. Collect data used for EPI ghost correciton
+%
+% This script creates the file '3DEPImultishot_cal.seq', that can be executed directly
+% on Siemens MRI scanners using the Pulseq interpreter.
+% The .seq file can also be converted to a .tar file that can be executed on GE
+% scanners, see main.m.
+%
+% The experimental parameters below are chosen such that the sequence 
+% can be executed identically (to us precision) on Siemens and GE systems.
+% For more information about preparing a Pulseq file for execution on GE scanners,
+% see the 'Pulseq on GE' manual.
+%
+% Performance?
+% Low temporal resolution (1s) but
+% High temporal resolution (1mm isotropic)
+%% Path and options
+seqname = '3DEPImultishot_cal';
+addpath('excitation/');
+caipiPythonPath = 'caipi/';
+
+%% Define experimental parameters
+sys = mr.opts('maxGrad', 40, 'gradUnit','mT/m', ...
+              'maxSlew', 120, 'slewUnit', 'T/m/s', ...
+              'rfDeadTime', 100e-6, ...
+              'rfRingdownTime', 60e-6, ...
+              'adcDeadTime', 20e-6, ...
+              'adcRasterTime', 2e-6, ...
+              'gradRasterTime', 10e-6, ...
+              'blockDurationRaster', 10e-6, ...
+              'B0', 3.0);
+sysGE = toppe.systemspecs('maxGrad', sys.maxGrad/sys.gamma*100, ...   % G/cm
+    'maxSlew', sys.maxSlew/sys.gamma/10, ...           % G/cm/ms
+    'maxRF', 0.25);
+
+% Basic parameters
+Nx = 200; Ny = Nx; Nz = 3;          % Matrix sizes
+fov = [200, 200, 3]*1e-3;           % field of view
+Nsegments = 4;                      % number of segments in EPI readout
+
+% Basic temporal parameters
+Nframes = 1;                        % number of temporal frames (image volumes)
+Ndummyframes = 4;                   % dummy frames to reach steady state
+
+% CAIPI sampling parameters
+Ry = 1;                             % ky undersampling factor
+Rz = 1;                             % kz undersampling factor
+CaipiShiftZ = 2;                    % Caipi shift factor
+pf_ky = 1.0;                        % partial Fourier factor along ky
+etl = ceil(pf_ky*Ny/Ry);            % echo train length
+NcyclesSpoil = 3;                   % number of Gx and Gz spoiler cycles
+
+% ADC stuff
+dwell = 4e-6;                       % ADC sample time (s). For GE, must be multiple of 2us.
+
+% Decay parameters
+TE = 30e-3;                         % echo time (s)
+volumeTR = 1000e-3;                 % temporal frame rate (s)
+zTR = volumeTR/Nz;                  % time to acquire a "slice" or z (s)
+TR = zTR/Nsegments;                 % time between excitations (s)
+T1 = 1500e-3;                       % T1 (s)
+
+% Excitation stuff
+alpha = 180/pi * acos(exp(-TR/T1)); % Ernst angle (degrees)
+rfDur = 8e-3;                       % RF pulse duration (s)
+rfTB  = 6;                          % RF pulse time-bandwidth product
+rfSpoilingInc = 117;                % RF spoiling increment (degrees)
+
+% Fat Sat Stuff
+fatChemShift = 3.5*1e-6;                        % 3.5 ppm
+fatOffresFreq = sys.gamma*sys.B0*fatChemShift;  % Hz
+
+%% Get CAIPI sampling pattern (for one shot/echo train)
+
+% create caipi.mat, and load it
+pyFile = [caipiPythonPath 'skippedcaipi_sampling.py'];
+pyCmd = sprintf('python3 %s %d %d %d %d %d %d', ...
+    pyFile, Ny, Nz, Ry, Rz, CaipiShiftZ, 1);
+
+% try to call Python script from Matlab
+pyenv(ExecutionMode="InProcess");
+[status, cmdout] = system(pyCmd, 'LD_PRELOAD', '/lib/x86_64-linux-gnu/libstdc++.so.6');
+if status == 1 % if failed
+    fprintf(cmdout);
+    fprintf('Open a terminal and run the following python command:\n\t%s\n', pyCmd);
+    input('Then press Enter to continue');
+end
+
+load caipi  
+
+% kz and ky indeces (multiples of deltak)
+kyInds = double(indices((end-etl+1):end, 2));
+kzInds = double(indices((end-etl+1):end, 1));
+
+% ky/kz encoding blip amplitude along echo train (multiples of deltak)
+kyStep = diff(kyInds);
+kzStep = diff(kzInds);
+
+%% Excitation pulse
+% Reusing Jon's SMS pulseq function for volume excitation
+mb = 1;
+slThick = fov(3);
+sliceSep = fov(3)/mb;   % center-to-center separation between SMS slices (m)
+[rf, gzRF, freq] = getsmspulse(alpha, slThick, rfTB, rfDur, ...
+    mb, sliceSep, sysGE, sys, ...
+    'doSim', false, ...    % Plot simulated SMS slice profile
+    'type', 'st', ...     % SLR choice. 'ex' = 90 excitation; 'st' = small-tip
+    'ftype', 'ls');       % filter design. 'ls' = least squares
+
+%% Fat sat pulse (TODO!!!)
+
+%% Define readout gradients and ADC event
+% The Pulseq toolbox really shines here!
+
+deltak = 1./fov;
+
+% Start with the blips
+gyBlip = mr.makeTrapezoid('y', sys, 'Area', max(abs(kyStep))*deltak(2)); 
+gzBlip = mr.makeTrapezoid('z', sys, 'Area', max(abs(kzStep))*deltak(3)); 
+
+% Area and duration of the biggest blip
+if gyBlip.area > gzBlip.area
+    maxBlipArea = gyBlip.area;
+    blipDuration = mr.calcDuration(gyBlip);
+else
+    maxBlipArea = gzBlip.area;
+    blipDuration = mr.calcDuration(gzBlip);
+end
+
+% Readout trapezoid
+systmp = sys;
+systmp.maxGrad = deltak(1)/dwell;  % to ensure >= Nyquist sampling
+gro = mr.makeTrapezoid('x', systmp, 'Area', Nx*deltak(1) + maxBlipArea);
+
+% ADC event
+Tread = mr.calcDuration(gro) - blipDuration;
+if mod(round(Tread*1e6)*1e-6, dwell)
+    Tread = Tread - mod(Tread, dwell) + dwell;
+end
+adc = mr.makeAdc(round(Tread/dwell), sys, ...
+    'Duration', Tread, ...
+    'Delay', blipDuration/2);
+
+% Split blips at block boundary.
+[gyBlipUp, gyBlipDown] = mr.splitGradientAt(gyBlip, mr.calcDuration(gyBlip)/2);
+gyBlipUp.delay = mr.calcDuration(gro) - mr.calcDuration(gyBlip)/2;
+gyBlipDown.delay = 0;
+
+[gzBlipUp, gzBlipDown] = mr.splitGradientAt(gzBlip, mr.calcDuration(gzBlip)/2);
+gzBlipUp.delay = mr.calcDuration(gro) - mr.calcDuration(gzBlip)/2;
+gzBlipDown.delay = 0;
+
+% prephasers and spoilers
+gxPre = mr.makeTrapezoid('x', sys, ...
+    'Area', -gro.area/2);
+Tpre = mr.calcDuration(gxPre);
+gyPre = mr.makeTrapezoid('y', sys, ...
+    'Area', (kyInds(1)-Ny/2)*deltak(2), ... 
+    'Duration', Tpre);
+gzPre = mr.makeTrapezoid('z', sys, ...
+    'Area', -Nz/2*deltak(3), ... 
+    'Duration', Tpre);
+gxSpoil = mr.makeTrapezoid('x', sys, ...
+    'Area', Nx*deltak(1)*NcyclesSpoil * (-1)^(Ny/Nsegments - 1));
+gzSpoil = mr.makeTrapezoid('z', sys, ...
+    'Area', Nx*deltak(1)*NcyclesSpoil * (-1)^(Ny/Nsegments - 1));
+
+%% Calculate delay to achieve desired TE
+kyIndAtTE = find(kyInds-Ny/2/Nsegments == min(abs(kyInds-Ny/2/Nsegments)));
+minTE = mr.calcDuration(gzRF) - mr.calcDuration(rf)/2 - rf.delay + ...
+        mr.calcDuration(gxPre) + ...
+        (kyIndAtTE-0.5) * mr.calcDuration(gro);
+TEdelay = floor((TE-minTE)/sys.blockDurationRaster) * sys.blockDurationRaster;
+
+%% Calculate delay to achieve desired TR
+minTR = mr.calcDuration(gzRF) + mr.calcDuration(gzPre)...
+                + Ny/Nsegments*mr.calcDuration(gro) + mr.calcDuration(gzSpoil);
+TRdelay = floor((TR - minTR)/sys.blockDurationRaster)*sys.blockDurationRaster;
+
+%% Assemble sequence
+seq = mr.Sequence(sys);           
+
+kyStepMax = max(abs(kyStep));
+kzStepMax = max(abs(kzStep));
+
+% RF spoiling stuff
+rf_phase = 0;
+rf_inc = 0;
+
+for frame = -Ndummyframes:0
+
+    % Convenience booleans for turning off adc and y gradient
+    isDummyFrame = frame < 0;
+    isCalFrame = frame == 0;
+    
+    % z-loop (move to proper kz location)
+    for z = 1:Nz
+        gzPreTmp = mr.scaleGrad(gzPre,ceil(Nz/2) - z);
+    
+        % In plane loop (2D segmented EPI)
+        for seg = 1:Nsegments
+            % Label the first block in each segment with the segment ID (see Pulseq on GE manual)
+            % !!This segment ID is NOT to be confused with the EPI segments!!
+            segmentID = 3*seg - (frame <= 0) - (frame == 0);
+    
+            % RF spoiling
+            rf.phaseOffset = rf_phase/180*pi;
+            adc.phaseOffset = rf_phase/180*pi;
+            rf_inc = mod(rf_inc+rfSpoilingInc, 360.0);
+            rf_phase = mod(rf_phase+rf_inc, 360.0);
+    
+            % RF excitation
+            seq.addBlock(rf, gzRF, mr.makeLabel('SET', 'TRID', segmentID));
+    
+            % TE delay
+            if TE > minTE
+                seq.addBlock(mr.makeDelay(TEdelay));
+            end
+
+            % Segment delay so TE is smooth along central line of k-space
+            segDelay = (seg - 1)*mr.calcDuration(gro);
+            if seg > 1
+                seq.addBlock(mr.makeDelay(segDelay));
+            end
+    
+            % Move to corner of k-space and sample the first line
+            gyPreSeg = mr.scaleGrad(gyPre, ((seg - 1)*gyBlip.area + gyPre.area)/gyPre.area);
+            if isDummyFrame
+                seq.addBlock(gxPre, gyPreSeg, gzPreTmp);
+                seq.addBlock(gro, ...
+                             mr.scaleGrad(gyBlipUp, Nsegments*kyStep(1)/kyStepMax));
+            elseif isCalFrame
+                seq.addBlock(gxPre);
+                seq.addBlock(gro, adc);
+            else
+                seq.addBlock(gxPre, gyPreSeg, gzPreTmp);
+                seq.addBlock(gro, adc,...
+                             mr.scaleGrad(gyBlipUp, Nsegments*kyStep(1)/kyStepMax));
+            end
+    
+            % Zip through k-space with EPI trajectory
+            for ie = 2:(etl/Nsegments - 1)
+                gybd = mr.scaleGrad(gyBlipDown, Nsegments*kyStep(ie-1)/kyStepMax);
+                gybu = mr.scaleGrad(gyBlipUp, Nsegments*kyStep(ie)/kyStepMax);
+                gybdu = mr.addGradients({gybd, gybu}, sys);
+                if isDummyFrame
+                    seq.addBlock(mr.scaleGrad(gro, (-1)^(ie-1)), gybdu);
+                elseif isCalFrame
+                    seq.addBlock(adc, mr.scaleGrad(gro, (-1)^(ie-1)));
+                else
+                    seq.addBlock(adc, mr.scaleGrad(gro, (-1)^(ie-1)), gybdu);
+                end
+            end
+    
+            % Last line
+            if isDummyFrame
+                seq.addBlock(mr.scaleGrad(gro, (-1)^(ie)), ...
+                             mr.scaleGrad(gyBlipDown, Nsegments*kyStep(ie)/kyStepMax));
+            elseif isCalFrame
+                seq.addBlock(adc, ...
+                             mr.scaleGrad(gro, (-1)^(ie)));
+            else
+                seq.addBlock(adc, ...
+                             mr.scaleGrad(gro, (-1)^(ie)), ...
+                             mr.scaleGrad(gyBlipDown, Nsegments*kyStep(ie)/kyStepMax));
+            end
+    
+            % spoil
+            seq.addBlock(gxSpoil, gzSpoil);
+    
+            % Achieve desired TR
+            seq.addBlock(mr.makeDelay(TRdelay - segDelay));
+        end
+    end
+end
+
+%% Check sequence timing
+[ok, error_report]=seq.checkTiming;
+if (ok)
+    fprintf('Timing check passed successfully\n');
+else
+    fprintf('Timing check failed! Error listing follows:\n');
+    fprintf([error_report{:}]);
+    fprintf('\n');
+end
+
+%% Output for execution
+seq.setDefinition('FOV', fov);
+seq.setDefinition('Name', seqname);
+seq.write(strcat(seqname, '.seq'));
+
+%% GE stuff
+seq2ge(strcat(seqname, '.seq'), sysGE, strcat(seqname, '.tar'))
+system(sprintf('tar -xvf %s', strcat(seqname, '.tar')));
+figure; toppe.plotseq(sysGE, 'timeRange',[0, volumeTR]);
+
+%% Detailed check that takes some time to run
+doDetailedCheck = false;
+if doDetailedCheck
+    % k-space trajectory calculation and plot
+    [ktraj_adc, t_adc, ktraj, t_ktraj, t_excitation, t_refocusing] = seq.calculateKspacePP();
+    figure; plot(ktraj(1,:),ktraj(2,:),'b'); % a 2D k-space plot
+    axis('equal'); % enforce aspect ratio for the correct trajectory display
+    hold;plot(ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % plot the sampling points
+    title('full k-space trajectory (k_x x k_y)');
+    
+    % Optional slow step, but useful for testing during development,
+    % e.g., for the real TE, TR or for staying within slewrate limits
+    rep = seq.testReport;
+    fprintf([rep{:}]);
+end
---
+++ recon3Dsa.m
@@ -0,0 +1,171 @@
+%% Reconstruction code using scan archives instead of p-files
+% Requires GE Orchestra
+% Rex Fung, June 6th, 2024
+
+%% User-defined values (CHANGE THESE)
+% Scanner system info
+sys = mr.opts('maxGrad', 40, 'gradUnit','mT/m', ...
+              'maxSlew', 120, 'slewUnit', 'T/m/s', ...
+              'rfDeadTime', 100e-6, ...
+              'rfRingdownTime', 60e-6, ...
+              'adcDeadTime', 20e-6, ...
+              'adcRasterTime', 2e-6, ...
+              'gradRasterTime', 10e-6, ...
+              'blockDurationRaster', 10e-6, ...
+              'B0', 3.0);
+sysGE = toppe.systemspecs('maxGrad', sys.maxGrad/sys.gamma*100, ...   % G/cm
+    'maxSlew', sys.maxSlew/sys.gamma/10, ...           % G/cm/ms
+    'maxRF', 0.25);
+
+% Redefine parameters for convenience
+fov = [200, 200, 3]*1e-3;
+Nx = 200; Ny = Nx; Nz = 3;
+Nsegments = 4;
+Nframes = 120;
+Ncoils = 32;
+
+% Filenames and options
+fn_cal = '/mnt/storage/rexfung/20240604brainstem3DEPI/scanArchives/3DEPI_cal/data.h5';
+fn_loop = '/mnt/storage/rexfung/20240604brainstem3DEPI/scanArchives/3DEPI_loop_v2/data.h5';
+fn_adc = sprintf('adc/P%dadc.mod', Nx);
+doSENSE = false;
+
+%% Data loading
+% Load raw data from scan archives (takes some time)
+mode = 1;
+switch mode
+    case 1
+        ksp_raw_cal = toppe.utils.loadsafile(fn_cal,'acq_order',true);
+        ksp_raw = toppe.utils.loadsafile(fn_loop,'acq_order',true);
+    case 2
+        ksp_raw_cal = read_arc(fn_cal);
+        ksp_raw = read_arc(fn_loop);
+end
+%% Preprocessing
+% Print max real and imag parts to check for reasonable magnitude
+fprintf('Max real part: %d\n', max(real(ksp_raw(:))))
+fprintf('Max imag part: %d\n', max(imag(ksp_raw(:))))
+
+% Reshape and permute calibration data (a single frame w/out blips)
+ksp_cal = flip(ksp_raw_cal, 1); % tv6 flips data along FID direction
+[Nfid,Ncoils,N] = size(ksp_cal);
+ksp_cal = ksp_cal(:,:,1:Ny*Nz); % discard trailing data
+ksp_cal = reshape(ksp_cal, [Nfid, Ncoils, Ny, Nz]);
+ksp_cal = permute(ksp_cal, [1 3 4 2]); % [Nfid Ny Nz Ncoils]
+
+% Reshape and permute loop data
+ksp_rampsamp = flip(ksp_raw, 1); % tv6 flips data along FID direction
+[Nfid,Ncoils,N] = size(ksp_rampsamp);
+ksp_rampsamp = ksp_rampsamp(:,:,1:Ny*Nz*Nframes);
+ksp_rampsamp = reshape(ksp_rampsamp, Nfid, Ncoils, Ny, Nz, Nframes);
+ksp_rampsamp = permute(ksp_rampsamp, [1 3 4 5 2]); % [Nfid Ny Nz Nframes Ncoils]
+
+% Rearrange segments/shots into proper locations
+ksp_cal_reordered = zeros(size(ksp_cal));
+ksp_rampsamp_reordered = zeros(size(ksp_rampsamp));
+for seg = 1:Nsegments
+    range = (1:Ny/Nsegments) + (seg-1)*Ny/Nsegments;
+
+    tmp1 = ksp_cal(:,range,:,:,:);
+    tmp1(:,1:2:end,:,:,:) = flip(tmp1(:,1:2:end,:,:,:),1);
+    ksp_cal_reordered(:,seg:Nsegments:end,:,:,:) = tmp1;
+
+    tmp2 = ksp_rampsamp(:,range,:,:,:);
+    tmp2(:,1:2:end,:,:,:) = flip(tmp2(:,1:2:end,:,:,:),1);
+    ksp_rampsamp_reordered(:,seg:Nsegments:end,:,:,:) = tmp2;
+end
+ksp_cal = ksp_cal_reordered;
+ksp_rampsamp = ksp_rampsamp_reordered;
+clear tmp1 tmp2 ksp_cal_reordered ksp_rampsamp_reordered;
+
+% Combine y and z dimensions as blips are off for cal data
+ksp_cal = reshape(ksp_cal, [Nfid, Ny, Nz*Ncoils]);
+
+% Naively recon each frame individually
+if true
+    imgs_naive = zeros(Nfid, Ny, Nz, Nframes);
+    parfor frame = 1:Nframes
+        fprintf('Naively reconstructing frame %d\n',round(frame))
+        ksp_frame = squeeze(ksp_rampsamp(:,:,:,frame,:));
+        img_frame_mc = toppe.utils.ift3(ksp_frame);
+        imgs_naive(:,:,:,frame) = sqrt(sum(abs(img_frame_mc).^2,4));
+    end
+end
+
+%% Prepare for reconstruction
+% Estimate k-space center offset due to gradient delay
+[M, I] = max(abs(ksp_cal),[],1);
+I = squeeze(I);
+delay = mean(I(1:Ny/2,:),'all') - Nfid/2;     
+fprintf('Estimated offset from center of k-space (samples): %f\n', delay);
+
+% retrieve sample locations from .mod file with adc info
+[rf,gx,gy,gz,desc,paramsint16,pramsfloat,hdr] = toppe.readmod(fn_adc);
+[kxo, kxe] = toppe.utils.getk(sysGE, fn_adc, Nfid, delay);
+
+% EPI ghost correction phase offset values
+oephase_data = ksp_cal;
+oephase_data(:,1:2:end,:,:) = flip(oephase_data(:,1:2:end,:,:),1); % Simulate 1-shot EPI
+oephase_data = hmriutils.epi.rampsampepi2cart(oephase_data, kxo, kxe, Nx, fov(1)*100, 'nufft');
+oephase_data = ifftshift(ifft(fftshift(reshape(oephase_data, [Nx, Ny, Nz*Ncoils])),Nx,1));
+[a, th] = hmriutils.epi.getoephase(oephase_data,true);
+fprintf('Constant phase offset (radians): %f\n', a(1));
+fprintf('Linear term (radians/fov): %f\n', a(2));
+
+%% Reconstruct multicoil k-space (each frame individually)
+
+ksp_mc = zeros(Nx, Ny, Nz, Nframes, Ncoils);
+parfor frame = 1:Nframes
+    fprintf('Reconstructing multicoil k-space of frame %d\n', round(frame))
+
+    % Extract one frame and rearrange to simulate 1-shot EPI 
+    ksp_rampsamp_frame = ksp_rampsamp(:,:,:,frame,:);
+    ksp_rampsamp_frame(:,1:2:end,:,:) = flip(ksp_rampsamp_frame(:,1:2:end,:,:),1);
+   
+    % grid
+    ksp_frame = hmriutils.epi.rampsampepi2cart(ksp_rampsamp_frame, kxo, kxe, Nx, fov(1)*100, 'nufft');
+
+    % phase correct
+    ksp_frame = hmriutils.epi.epiphasecorrect(ksp_frame, a);
+
+    % Allocate
+    ksp_mc(:,:,:,frame,:) = ksp_frame;
+end
+
+%% Get sensitivity maps with PISCO
+if doSENSE
+    addpath('pisco/');
+    ksp4maps = squeeze(ksp(:,:,:,2,:));
+    ksp4maps = ifftshift(ifft(fftshift(ksp4maps),Nz,3));
+    smaps = zeros(Nx, Ny, Nz, Ncoils);
+    for z = 1:Nz
+        [smaps(:,:,z,:), eigvals] = PISCO_senseMaps_estimation(squeeze(ksp4maps(:,:,z,:)), [Nx, Ny]);
+    end
+
+    % Add a time dimension so it can be multiplied
+    smaps = reshape(smaps,[Nx Ny Nz 1 Ncoils]);
+end
+
+%% IFFT to get images
+imgs_mc = ifftshift(ifft(...
+                     ifft(...
+                      ifft(...
+                       fftshift(ksp_mc)...
+                       , Nx, 1)...
+                      , Ny, 2)...
+                     , Nz, 3)...
+                    );
+
+%% Coil combination
+if doSENSE
+    imgs = sum(imgs_mc .* conj(smaps), 5);
+else % root sum of squares combination
+    imgs = sqrt(sum(abs(imgs_mc).^2, 5));
+end
+
+return;
+%% Viz
+z = 2;
+figure; im(imgs(:,:,z,:),'cbar')
+title(fn_loop(1:end-2));
+% saveas(gcf, strcat('figs/',fn(1:end-2),'.png'));
